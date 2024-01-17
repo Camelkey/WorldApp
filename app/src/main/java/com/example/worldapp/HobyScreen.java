4b549ba792a6b0b5d49e9d684a12c60a7125c779
WorldApp <huseyinburak.tetikk@gmail.com>
2024-01-17 16:31:38 +0300

    hobbies screen update

--- app/src/main/java/com/example/worldapp/HobyScreen.java
+++ app/src/main/java/com/example/worldapp/HobyScreen.java
@@ -10,5 +10,14 @@ public class HobyScreen extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_hoby_screen);
+
+        //Pull  Data
+        String username = getIntent().getStringExtra("username");
+        String password = getIntent().getStringExtra("password");
+        String email = getIntent().getStringExtra("email");
+
+        // Take Hobbies
+
+
     }
 }
--- app/src/main/java/com/example/worldapp/MainActivity.java
+++ app/src/main/java/com/example/worldapp/MainActivity.java
@@ -2,14 +2,19 @@ package com.example.worldapp;
 
 
 import android.annotation.SuppressLint;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 
 public class MainActivity extends AppCompatActivity {
 
-
+    private EditText emailEditText, passwordEditText, userNameEditText;
+    private Button registerButton;
 
 
 
@@ -19,15 +24,45 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        userNameEditText = findViewById(R.id.userNameText);
+        emailEditText = findViewById(R.id.emailText);
+        passwordEditText = findViewById(R.id.passwordText);
+        registerButton = findViewById(R.id.registerButton);
+
+        registerButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String username = userNameEditText.getText().toString();
+                String email = emailEditText.getText().toString();
+                String password = passwordEditText.getText().toString();
 
 
-
-
-
+                Intent intent = new Intent(MainActivity.this,HobyScreen.class);
+                intent.putExtra("username",username);
+                intent.putExtra("password", password);
+                intent.putExtra("email", email);
+                startActivity(intent);
 
             }
+        });
+    }
+}
 
-                }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
